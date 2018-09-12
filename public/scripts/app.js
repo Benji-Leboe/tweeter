f6a3426e5a7274ed58fb576da6e7e9d6e4fb1161
Benji-Leboe <benjamin.r.leboe@gmail.com>
2018-09-12 02:34:24 +0000

    got tweet array output

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -18,11 +18,59 @@ let tweetObj = {
   },
   "created_at": 1461116232227
 }
+const newArr = [1,2,3,4,5];
+
+const tweetArr = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
 
 
 $(function(){
   composerCount();
-  tweetListener(tweetObj);
+  tweetListener(tweetArr);
 });
 
 function composerCount(){
@@ -90,16 +138,24 @@ function createTweetElement(tweetObj) {
   return $article;
 }
 
-function tweetListener(tweetObj){
+// function tweetListener(tweetObj){
+//   $("#submit-form").on('submit', function(event){
+//     event.preventDefault();
+//     // $("#submit-form").val();
+//     console.log(typeof createTweetElement(tweetObj));
+//     console.dir(createTweetElement(tweetObj));
+//     $(".tweets").prepend(createTweetElement(tweetObj));
+    
+//   });
+// }
+
+
+
+function tweetListener(tweetArr){
   $("#submit-form").on('submit', function(event){
     event.preventDefault();
-    // $("#submit-form").val();
-    $(".tweets").prepend(createTweetElement(tweetObj));
-  });
-}
-
-function renderTweets(tweetArr){
-  tweetArr.forEach(function(tweet){
-    $("#tweet-container").prepend(createTweetElement(tweet));
+    for(let tweet of tweetArr){
+      $(".tweets").prepend(createTweetElement(tweet));
+    }
   });
 }
